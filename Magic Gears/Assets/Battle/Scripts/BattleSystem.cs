ad9ac27eb7967fdbb12b018e07b1d8fea32a0da4
SouloCider <luke.wittmayer@knights.ucf.edu>
2023-10-04 01:04:16 -0400

    Adds Scripts
    Adds Unit and BattleSystem scripts. Still very much a work in progress.

---
+++ Magic Gears/Assets/Battle/Scripts/BattleSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+    }
+
+
+}

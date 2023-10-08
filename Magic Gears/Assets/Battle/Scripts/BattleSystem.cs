153f6707f468d8f715ada859b7c91ed2c1d9ce5c
SouloCider <luke.wittmayer@knights.ucf.edu>
2023-10-08 16:20:04 -0400

    Update Combat Scene Script

--- Magic Gears/Assets/Battle/Scripts/BattleSystem.cs
+++ Magic Gears/Assets/Battle/Scripts/BattleSystem.cs
@@ -6,12 +6,24 @@ public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
     public BattleState state;
 
     // Start is called before the first frame update
     void Start()
     {
         state = BattleState.START;
+        SetUpBattle();
+    }
+
+    void SetUpBattle()
+    {
+        Instantiate(playerPrefab, playerBattleStation);
+        Instantiate(enemyPrefab, enemyBattleStation);
     }
 
 

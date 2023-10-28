5bd365120d99dd945f171d85c096e794eb66a8af
L7R-a <gedylarosa@gmail.com>
2023-10-28 18:51:40 -0400

    Implemented dialogue mechanic for the beginning of each level
    Leaving the hub to chapter/level 1 will have first a dialogue scene and then run the actual combat.
    Also, organized the folders of the project

---
+++ Magic Gears/Assets/Scripts/Dialogue/LevelConversation.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using DialogueEditor;
+using UnityEngine.SceneManagement;
+
+public class LevelConversation : MonoBehaviour
+{
+    [SerializeField] private NPCConversation conversation;
+    [SerializeField] private int teleportTo;
+
+    private CharacterClass characterClass; //Used to stop player from moving
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start conversation and stop player from moving
+        ConversationManager.Instance.StartConversation(conversation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        ConversationManager.OnConversationStarted += ConversationStart;
+        ConversationManager.OnConversationEnded += ConversationEnd;
+    }
+
+    private void OnDisable()
+    {
+        ConversationManager.OnConversationStarted -= ConversationStart;
+        ConversationManager.OnConversationEnded -= ConversationEnd;
+    }
+    private void ConversationStart()
+    {
+        Debug.Log("A conversation has began.");
+    }
+
+    private void ConversationEnd()
+    {
+        if (teleportTo == 1)
+        {
+            SceneManager.LoadScene("Combat Scene - Luke");
+        }
+        else if (teleportTo == 2)
+        {
+            SceneManager.LoadScene("Hub");
+        }
+        else if (teleportTo == 3)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
---
+++ Magic Gears/Assets/Scripts/Hub/SceneChange.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(int level)
+    {
+        if(level == 1)
+        {
+            SceneManager.LoadScene("Dialogue1.1");
+        }
+        else if (level == 2)
+        {
+            SceneManager.LoadScene("OLD_Hub");
+        }
+        else if (level == 3)
+        {
+            SceneManager.LoadScene("OLD_Hub");
+        }
+        else if (level == 0)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+    }
+}
